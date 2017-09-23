c289403358dc43a746c467f4e3b7b7b9c965180a
espressohn <espressohn@gmail.com>
2017-09-23 21:54:47 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,25 +1,25 @@
 function basicTeenager(age) {
   if (age>=13 && age=<19){
-    return("You are a teenager!")
+    return "You are a teenager!"
   }
   return null
 }
 
 function teenager(age) {
   if (age>=13 && age=<19){
-    return("You are a teenager!")
+    return "You are a teenager!"
   }else {
-    return ("You are not a teenager")
+    return "You are not a teenager"
   }
 }
 
 function ageChecker(age) {
   if (age<13){
-    return("You are a kid")
+    return "You are a kid"
   }else if (age<19){
-    return ("You are a teenager!")
+    return "You are a teenager!"
   }else {
-    return ("You are a grown up")
+    return "You are a grown up"
   }
 }
 
